ed166ebc28599e3e6f00e47ac96d9b00ee12ba79
Aigerim <aigerim4334@gmail.com>
2022-10-13 00:21:19 +0300

    fdfsdfs:: fdsf

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  	import NavBar from '../components/NavBar.svelte';
+  	import Contact from '../components/Contact.svelte';
+	import './styles.css';
+</script>
+
+<NavBar />
+
+<slot />
+
+<Contact />
