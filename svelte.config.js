a9d88265030148120289a62e8408e5e0f2edba5b
Aigerim <aigerim4334@gmail.com>
2022-10-13 02:26:52 +0300

    fxfxfx

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 	},
 	// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
 	// For example, instead of '_app', use 'app_', 'internal', etc.
-	app__: 'internal',
+	appDir: '_app',
 };
 
 export default config;
