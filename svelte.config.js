abc99ce16d2299d81a274b096e46a13b92c1e27d
Aigerim <aigerim4334@gmail.com>
2022-10-13 02:16:45 +0300

    fxfxfx

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+const dev = process.env.NODE_ENV === 'development';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,12 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+	paths: {
+		base: dev ? '' : '/aigerimu.github.io',
+	},
+	// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+	// For example, instead of '_app', use 'app_', 'internal', etc.
+	appDir: 'internal',
 };
 
 export default config;
