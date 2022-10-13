e7946cc2447e6b3a8fc6ac2ab8057c9839151354
Aigerim <aigerim4334@gmail.com>
2022-10-13 21:31:07 +0300

    fdfs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,14 @@
 	import './styles.css';
 </script>
 
+<svelte:head>
+	<link rel="stylesheet" href="global.css" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <NavBar />
 
 <slot />
