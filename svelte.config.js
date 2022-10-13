1ebcd1b01f35438e1c707b7c066409f452b2d152
Aigerim <aigerim4334@gmail.com>
2022-10-13 18:07:09 +0300

    fdfsd

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,24 @@
+// const dev = process.env.NODE_ENV === 'development';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+
 	preprocess: preprocess(),
 
 	kit: {
 		adapter: adapter(),
 		// prerender: {
+		// 	// entries:[],
 		// 	default: true
-		// }
-		// prerender: {
-		// 	entries: ['/episodes-[number]'],
 		// },
-		
+		// paths: {
+		// 	base: dev ? '' : '/Aigerim',
+		// },
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal',
 	}
 };
 
