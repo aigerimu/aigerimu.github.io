dbb53a86dc1f4327eee5de955b2f81855ecdefa2
Aigerim <aigerim4334@gmail.com>
2022-10-13 21:33:45 +0300

    fdfs

--- src/routes/randog/+page.svelte
+++ src/routes/randog/+page.svelte
@@ -2,4 +2,12 @@
 	import RanDog from '../../components/RanDog.svelte';
 </script>
 
+<svelte:head>
+	<link rel="stylesheet" href="global.css" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <RanDog />
